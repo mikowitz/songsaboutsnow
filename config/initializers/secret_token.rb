0812aa0745f64eeb700e1f43cc574472858c3cea
Michael Berkowitz <michael.berkowitz@gmail.com>
2014-02-11 16:28:19 -0500

    Remove Rails app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Songsaboutsnow::Application.config.secret_token = '43ab5617d0c2dbed1cbb1ab359e07b44a4913926d039d0c03e104beed826776f8914d2c5552c49b33e5b18aafbe9794b996a930cb55612384a00752f20aff91b'
