b6d323be8a953adf200af137d449bfbd06688d03
Michael Berkowitz <michael.berkowitz@gmail.com>
2014-12-11 13:26:59 -0500

    haml!

--- songsaboutsnow.rb
+++ songsaboutsnow.rb
@@ -1,24 +1,20 @@
-require 'sinatra/base'
-require 'sinatra/asset_snack'
+require "sinatra/base"
+require "sinatra/asset_snack"
 
 class Songsaboutsnow < Sinatra::Base
   register Sinatra::AssetSnack
-  asset_map '/styles/application.css', ['assets/css/*.scss']
-  get '/' do
-    send_file 'views/index.html'
+  asset_map "/styles/application.css", ["assets/css/*.scss"]
+  get "/" do
+    @poetry_links = {
+      "One Sentence Poems" => "http://onesentencepoems.com/osp/michael-berkowitz-the-restored-typewriter/",
+      "The Rampallian" => "http://www.magcloud.com/browse/issue/804364",
+      "Eunoia Review" => "https://eunoiareview.wordpress.com/tag/michael-berkowitz",
+      "Sixfold" => "http://www.sixfold.org/PoWinter13/Berkowitz.html"
+    }
+    haml :test
   end
 
   get "/penguins" do
     haml :penguins
   end
-
-  #get "/test" do
-    #@poetry_links = {
-      #"One Sentence Poems" => "http://onesentencepoems.com/osp/michael-berkowitz-the-restored-typewriter/",
-      #"The Rampallian" => "http://www.magcloud.com/browse/issue/804364",
-      #"Eunoia Review" => "https://eunoiareview.wordpress.com/tag/michael-berkowitz",
-      #"Sixfold" => "http://www.sixfold.org/PoWinter13/Berkowitz.html"
-    #}
-    #haml :test
-  #end
 end
